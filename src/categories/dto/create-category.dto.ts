f4812c69af1b9b52d3a09273782afb9c617491e2
Наконечный Антон Викторович <anakonechny@alfabank.ru>
2023-03-20 12:19:03 +0300

    feat: add category type

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,6 +1,11 @@
-import { IsString } from 'class-validator';
+import { CategoryType } from '@prisma/client';
+import { IsString, IsEnum, IsOptional } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
     name: string;
+
+  @IsEnum(CategoryType)
+  @IsOptional()
+    type?: CategoryType;
 }
